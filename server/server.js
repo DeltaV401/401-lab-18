f5d49fe3df952cc807d96974613a6663e1279851
Jon Struve <ocdammo3@gmail.com>
2019-10-09 14:23:44 -0500

    Added index code in logger and socketio code in server

--- server/server.js
+++ server/server.js
@@ -1,35 +1,21 @@
 'use strict';
 
-const net = require('net');
+const socketIO = require('socket.io');
 const uuid = require('uuid');
 
 const PORT = process.env.PORT || 3000;
-const server = net.createServer();
-
-server.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}.`);
-});
-
-const socketPool = {};
+const server = socketIO(PORT);
+console.log(`Listening on port ${PORT}.`);
 
 server.on('connection', socket => {
-  const id = uuid();
-  socket.id = id;
-  socketPool[id] = socket;
+  console.log(`You're connected!`, socket.id);
 
-  console.log(`${id} connected to the party! ${Object.keys(socketPool).length} people are now partying!`);
-
-  for(let socketId in socketPool) {
-    socketPool[socketId].write(`${id} connected to the party!\r\n`);
-  }
-
-  socket.on('data', dataHandler);
   socket.on('error', err => {
     console.error(err);
   });
-  socket.on('close', () => {
-    console.log(`Kicking ${id} out for trying to leave. ${Object.keys(socketPool).length} people still know how to party.`);
-    delete socketPool[id];
+
+  socket.on('send-chat', data => {
+    server.emit('chat', data);
   });
 });
 
